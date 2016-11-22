77c1a7556a610236bba6439992e877dabe1e6bf5
Yanga93 <yangazukelwa@gmail.com>
2016-11-22 16:21:10 +0200

    Returns regNum of string for Paarl

---
+++ countAllPaarl.js
@@ -0,0 +1,16 @@
+const assert = require('assert');
+  var countAllPaarl = function(regList){
+    var regNum = regList.split(", ");
+
+      var countPaarl = [];
+        for(var i=0; i<regNum.length; i++){
+
+if (regNum[i].startsWith('CJ')){
+    countPaarl.push(regNum[i]);
+}
+  }
+  console.log(countPaarl.length);
+    return countPaarl;
+};
+  var result = countAllPaarl("CJ 3625, CA 4615, DR 12 TY GP, CJ 5636, DRT 122 L");
+    //assert.equal(result, 2);
